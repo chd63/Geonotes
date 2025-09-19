b1d8c5626f7f82862ea088a8b87a3e205bbe16a6
chd63 <chd63@nau.edu>
2025-09-19 04:17:09 -0700

    added zoom in on click that does not really work, plan is to get rid of it and make seprate pages open

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import {
@@ -10,14 +10,48 @@ import {
 
 // Official world countries GeoJSON
 const geoUrl =
-  "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+  "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json";
 
-function handleClick(geo) {
-  alert(`You clicked on ${geo.properties.name}`);
+
+
+function handleClick(geo,setPosition,setSelectedCountry) {
+  handleZoomIn(geo,setPosition,setSelectedCountry);
   console.log(geo);
 }
 
+function handleZoomIn(geo,setPosition,setSelectedCountry) {
+  // we are going to need some logic to look at the name of the
+  // country and then set the coordinates to the centroid of that country
+  // we also need to set zoom level according to country size
+  let estimated_centroid = [0,0];
+
+
+  try{
+    estimated_centroid = geo.geometry.coordinates[0][0];
+  }
+  catch(e){
+    console.log(arr);
+    estimated_centroid = geo.geometry.coordinates[0][0][0][0];
+  }
+
+
+  console.log(estimated_centroid);
+  
+  const centroid =  estimated_centroid ;
+  const newZoom = 5; // Example zoom level
+
+  setPosition({
+    coordinates: centroid,
+    zoom: newZoom,
+  });
+  setSelectedCountry(geo.properties.name);
+}
+
 function App() {
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  
 
   return (
     <div className="center-container">
@@ -33,7 +67,10 @@ function App() {
           strokeWidth="3"
           fill="none"
       />
-      <ZoomableGroup>
+      <ZoomableGroup
+      zoom={position.zoom}
+      center={position.coordinates}
+      onMoveEnd={(newPosition) => setPosition(newPosition)}>
 
       <Geographies geography={geoUrl}>
         {({geographies}) =>
@@ -41,7 +78,7 @@ function App() {
               <Geography 
                 key={geo.rsmKey} 
                 geography={geo} 
-                onClick={() => handleClick(geo)}
+                onClick={() => handleClick(geo,setPosition,setSelectedCountry)}
                 style = {{
                   default: { fill: "#D6D6DA", outline: "none" },
                   hover: { fill: "#F53", outline: "none" },
@@ -55,10 +92,15 @@ function App() {
       </ZoomableGroup>
       </ComposableMap> 
     
-    
- 
-      
-    </div> 
+      </div> 
+      <div className="notes-container">
+        <h2>Notes on {selectedCountry || "Selected Country"}</h2>
+        <textarea
+          placeholder="Add your notes here..."
+          rows="10"
+          cols="30"
+        />
+      </div>
     </div>
   );
 }
